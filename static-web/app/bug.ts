62f55e9cdca81466ba6d60a88f764e7288dce04a
Thomas Spycher <me@tspycher.com>
2017-01-05 21:53:58 +0100

    updated url

--- static-web/app/bug.ts
+++ static-web/app/bug.ts
@@ -14,37 +14,37 @@ export class BugService {
   }
 
   toggleLowBeamLight() {
-    var url = "http://127.0.0.1:5000/cmd/l";
+    var url = "/cmd/l";
     return this.http.get(url).map(res => res.json());
   }
 
   toggleHeadLight() {
-    var url = "http://127.0.0.1:5000/cmd/h";
+    var url = "/cmd/h";
     return this.http.get(url).map(res => res.json());
   }
 
   doFlash() {
-    var url = "http://127.0.0.1:5000/cmd/f";
+    var url = "/cmd/f";
     return this.http.get(url).map(res => res.json());
   }
 
   toggleTurnLightLeft() {
-    var url = "http://127.0.0.1:5000/cmd/o";
+    var url = "/cmd/o";
     return this.http.get(url).map(res => res.json());
   }
 
   toggleTurnLightRight() {
-    var url = "http://127.0.0.1:5000/cmd/p";
+    var url = "/cmd/p";
     return this.http.get(url).map(res => res.json());
   }
 
   toggleWarningLights() {
-    var url = "http://127.0.0.1:5000/cmd/w";
+    var url = "/cmd/w";
     return this.http.get(url).map(res => res.json());
   }
 
   status() {
-    var url = "http://127.0.0.1:5000/status";
+    var url = "/status";
     return this.http.get(url).map(res => res.json());
   }
 }
